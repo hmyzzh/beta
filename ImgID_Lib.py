838e587e2e818605a67fe42f8eecabe808371155
hmyzzh <z479412411@gmail.com>
2023-03-12 16:02:32 -0600

    uodate

--- ImgID_Lib.py
+++ ImgID_Lib.py
@@ -1,6 +1,21 @@
 dictt={
     "IMG0001":"http://i.endpot.com/image/SGBF5/MStRdmdPRFIrRjFhdklMdTRsUm94eFdxMWNGa0RKeGcvdVBZQmJNd0gvND0.jpg",
     "IMG0002":"https://i.endpot.com/image/5N7AM/IMG_0389.JPEG",
+
+    # King Ricard's Faire
+    "Post_20220929_01":"https://live.staticflickr.com/65535/52743607989_6bd0eba30c_k.jpg",
+    "Post_20220929_02":"https://live.staticflickr.com/65535/52743847053_a644441085_k.jpg",
+    "Post_20220929_03":"https://live.staticflickr.com/65535/52743772815_c64000dc4c_k.jpg",
+    "Post_20220929_04":"https://live.staticflickr.com/65535/52743772590_4aa4b298dc_k.jpg",
+    "Post_20220929_05":"https://live.staticflickr.com/65535/52743846618_f19e399485_k.jpg",
+    "Post_20220929_06":"https://live.staticflickr.com/65535/52743772365_1d5762a6d3_k.jpg",
+    "Post_20220929_07":"https://live.staticflickr.com/65535/52743772260_cf81ffbe6e_k.jpg",
+    "Post_20220929_08":"https://live.staticflickr.com/65535/52743772140_f5bad67d50_k.jpg",
+    "Post_20220929_09":"https://live.staticflickr.com/65535/52742838877_fe833ef6e9_k.jpg",
+    "Post_20220929_10":"https://live.staticflickr.com/65535/52743606579_58f38b58c8_k.jpg",
+    "Post_20220929_11":"https://live.staticflickr.com/65535/52743771780_51310cda41_k.jpg",
+
+    # farewell dazhu
     "Post_20230111_01":"https://live.staticflickr.com/65535/52719026020_71d94a3b15_k.jpg",
     "Post_20230111_02":"https://live.staticflickr.com/65535/52743460800_e985209661_k.jpg",
     "Post_20230111_03":"https://live.staticflickr.com/65535/52743460630_5a2c48a6fd_k.jpg",
@@ -11,6 +26,8 @@ dictt={
     "Post_20230111_08":"https://live.staticflickr.com/65535/52742528957_53dc36c4c9_k.jpg",
     "Post_20230111_09":"https://live.staticflickr.com/65535/52743297109_31c5cbcf86_k.jpg",
     "Post_20230111_10":"https://live.staticflickr.com/65535/52743459615_26e7c5c999_h.jpg",
+
+    # Denver zoo
     "Post_20230129_03":"https://live.staticflickr.com/65535/52711452795_b5233186ec_k.jpg",
     "Post_20230129_02":"https://live.staticflickr.com/65535/52711514698_eae834868b_h.jpg",
     "Post_20230129_01":"https://live.staticflickr.com/65535/52711036546_cd09af0fab_h.jpg",
@@ -22,6 +39,8 @@ dictt={
     "Post_20230129_09":"https://live.staticflickr.com/65535/52719015623_a93d51124e_k.jpg",
     "Post_20230129_10":"https://live.staticflickr.com/65535/52718951495_7b9c975850_k.jpg",
     "Post_20221022_01":"https://live.staticflickr.com/65535/52711961945_ece37d5b89_k.jpg",
+
+    # Golden Gate
     "Post_20221022_02":"https://live.staticflickr.com/65535/52711545221_1a997f65fc_k.jpg",
     "Post_20221022_03":"https://live.staticflickr.com/65535/52712021173_53f3b57015_k.jpg",
     "Post_20221022_04":"https://live.staticflickr.com/65535/52711961870_5b60dc4cb6_k.jpg",
@@ -45,6 +64,6 @@ dictt={
     "Post_20211010_16":"https://live.staticflickr.com/65535/52711974929_97b2971718_k.jpg",
     "Post_20211010_17":"https://live.staticflickr.com/65535/52712191813_d9b1377a63_k.jpg",
     "video_boston_troy":"https://live.staticflickr.com/65535/52743311186_0d362df580_h.jpg",
-    "video_NEA":"https://live.staticflickr.com/65535/52743563579_8c4dd0a4aa_k.jpg",
+    "video_NEA":"https://live.staticflickr.com/65535/52743563579_8c4dd0a4aa_k.jpg"
     
 }
